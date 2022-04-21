413d597d399c23690113492f845092178c2ebf7c
Hao Tang <hao.tang@unitn.it>
2022-04-21 14:39:32 +0200

    clear code

--- train.sh
+++ train.sh
@@ -1,1 +1,2 @@
-python train.py --name SPADE_StripPooling_cat_twolayer_ppooling4 --dataset_mode cityscapes --dataroot /data/engs-tvg/engs1993/SPADE/datasets/Cityscapes/data --niter 100 --niter_decay 100 --gpu_ids 0 --checkpoints_dir /data/engs-tvg/engs1993/checkpoints --batchSize 4 --save_epoch_freq 5  --save_latest_freq 1000 --continue_train;
+python train.py --name dpgan_city --dataset_mode cityscapes --dataroot ./datasets/Cityscapes/data --niter 100 --niter_decay 100 --gpu_ids 0 --checkpoints_dir ./checkpoints --batchSize 4 --save_epoch_freq 5  --save_latest_freq 1000 
+# --continue_train;
